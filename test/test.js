4d48c0d3ae31feebc5f23380d2960c50a2e26088
Adam Král <adam.kral@esmo.sk>
2020-09-02 15:42:11 +0200

    testy

--- test/test.js
+++ test/test.js
@@ -2,6 +2,9 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const tap = require('tap');
 
+const mock = x => x.split("").map(x=>Math.random()<1/2?x.toLowerCase():x.toUpperCase()).join("");
+
+
 (async () => {
     let fdupesSorted, deduplicatorSorted;
     // you may encounter invalid number of args here, but it's actually valid as the promisify takes
@@ -18,13 +21,15 @@ const tap = require('tap');
         fdupesSorted = stdout.split("\n").filter(Boolean).sort();
     })();
     await (async () => {
-        const {stdout, stderr} = await exec('make main-notime', {cwd: "../"});
+        const {stdout, stderr} = await exec('make -s main-notime', {cwd: "../"});
         tap.ok(stdout, "Deduplicator stdout should be truthy");
         tap.notOk(stderr, "Deduplicator stderr should be falsey");
         deduplicatorSorted = stdout.split("\n").filter(Boolean).sort();
     })();
-    tap.ok(deduplicatorSorted, );
-    tap.equal(fdupesSorted, deduplicatorSorted, "Outputs should be equal");
+    tap.ok(deduplicatorSorted);
+    tap.same(fdupesSorted, deduplicatorSorted, "Outputs should be equal");
+
+    fdupesSorted.map((entry,index) => tap.same(entry, deduplicatorSorted[index], "entry should be equal"));
 
 
 })();
