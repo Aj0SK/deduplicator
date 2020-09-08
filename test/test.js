e9bb481ad7180bfa7736b8b8f2050498bcead721
Adam Král <adam@xn--krl-fla.com>
2020-09-08 22:14:46 +0200

    fix tests

--- test/test.js
+++ test/test.js
@@ -15,19 +15,19 @@ const presliTesty = true;
         const {stdout, stderr} = await exec('make -s create_test_data', {cwd: "../"});
         tap.ok(stdout);
         //tap.notOk(stderr);
-        cppSorted = stdout.split("\n").filter(Boolean).map(x=>x.trim()).split("").sort();
+        cppSorted = stdout.split("\n").filter(Boolean).map(x=>x.trim()).sort();
     })();
     await (async () => {
         const {stdout, stderr} = await exec('fdupes -r1q data', {cwd: "../"});
         tap.ok(stdout, "Fdupes stdout should be truthy");
         tap.notOk(stderr, "Fdupes stderr should be falsey");
-        fdupesSorted = stdout.split("\n").filter(Boolean).map(x=>x.trim()).split("").sort();
+        fdupesSorted = stdout.split("\n").filter(Boolean).map(x=>x.trim()).sort();
     })();
     await (async () => {
         const {stdout, stderr} = await exec('make -s main-notime', {cwd: "../"});
         tap.ok(stdout, "Deduplicator stdout should be truthy");
         tap.notOk(stderr, "Deduplicator stderr should be falsey");
-        deduplicatorSorted = stdout.split("\n").filter(Boolean).map(x=>x.trim()).split("").sort();
+        deduplicatorSorted = stdout.split("\n").filter(Boolean).map(x=>x.trim()).sort();
     })();
     tap.ok(deduplicatorSorted);
     tap.ok(cppSorted);
